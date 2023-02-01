b2c23d69a0dbf192d7134d42ac5cb299e5a67ce0
PNS212624\Hansol <yrlee00@hansol.com>
2023-02-01 10:36:35 +0900

    1/31 commit

---
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -0,0 +1,28 @@
+package com.example.exception.controller;
+
+import com.example.exception.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class ApiController {
+
+
+   //required = false는 파라미터가 없어도 변수는 null로 떨어짐
+    @GetMapping("")
+    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) int age){
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        int a = 10 + age;
+
+        return user;
+    }
+
+    @PostMapping("")
+    public User post(@RequestBody User user){
+        System.out.println(user);
+        return user;
+    }
+}
